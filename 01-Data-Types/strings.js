1d141d8c7f427a0c17af4d3d0b15d2b013f8f86c
Maruf Siddique <marufsiddique71@yahoo.com>
2022-01-20 18:44:15 +0600

    string basic

---
+++ 01-Data-Types/strings.js
@@ -0,0 +1,20 @@
+// String declaration
+
+let space = " "
+let firstName = 'Maruf'
+let lastName = 'Siddique'
+
+// String Concatenation
+
+let fullName = firstName +space+ lastName
+console.log(fullName)
+
+console.log(`My name is ${fullName}`)
+
+// \n = new line
+// \t = tab: 8 spaces
+// \\ : back slash
+// \' : single quote (')
+// \" : double quote (")
+
+
