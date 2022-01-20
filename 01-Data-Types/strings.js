2e06bcc47583d43935e441a3004d27913e04cfb2
Maruf Siddique <marufsiddique71@yahoo.com>
2022-01-20 19:02:49 +0600

    string o logy

--- 01-Data-Types/strings.js
+++ 01-Data-Types/strings.js
@@ -17,4 +17,39 @@ console.log(`My name is ${fullName}`)
 // \' : single quote (')
 // \" : double quote (")
 
+// String methods 
+
+let js = 'JavaScript'
+console.log(js.length)
+
+console.log(js.toUpperCase())
+console.log(js.toLowerCase())
+
+// sting slice 
+
+let string = 'Bangladesh'
+console.log(string.substr(0,6))
+
+// substring
+let anotherCountry = "Bangladesh"
+console.log(anotherCountry.substring(0,6))
+
+// Split method
+let cName = "Learn JavaScript"
+
+console.log(cName.split(' '))
+console.log(cName.split('a'))
+
+// trim method 
+let demoText = '      First day of JavaScript  '
+console.log(demoText.trim(' '))
+
+let test = 'some jibrish text'
+console.log(test.replace('text', 'lifee'))
+
+
+// charAt() method
+
+let index = 'demo man with demo gun'
+console.log(index.charAt(5))
 
