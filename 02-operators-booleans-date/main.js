94d4c317612814b7c1ff95ec851c124f26f5c5ac
Maruf Siddique <marufsiddique71@yahoo.com>
2022-01-21 21:42:09 +0600

    booleans

---
+++ 02-operators-booleans-date/main.js
@@ -0,0 +1,46 @@
+// undefined value
+
+let firstName 
+console.log(firstName)
+
+// null value
+let empty = null
+console.log(empty)
+
+// ------Operators-----
+// + - * / % **
+
+// -----Compararison Operators 
+// == === != > < <= <= 
+
+// Logical operators 
+// && || 
+
+// Increment operators 
+// ++ 
+
+// Decrement Operators
+// -- 
+
+// Ternary Operators
+// condition ? true : false
+
+
+// Window method
+// alert('welcome to js')
+
+// windows prompt()
+// prompt('input text', 'optional text')
+
+// let number = prompt('Enter Number', 'Input number here')
+// console.log(number)
+
+// date object
+const now = new Date()
+console.log(now)
+
+// getting full year
+
+
+
+
