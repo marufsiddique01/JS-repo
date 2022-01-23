525b8a33114535f9572037469d41acb5d0c1cf25
Maruf Siddique <marufsiddique71@yahoo.com>
2022-01-23 23:30:06 +0600

    for of loop

--- 05-loops/loops.js
+++ 05-loops/loops.js
@@ -20,4 +20,19 @@ while(i<=5){
   i++;
 }
 
+// do while loop
+
+let j = 1;
+do{
+  console.log(`goru number ${j}`)
+  j++
+}while(j <= 5)
+
+// for of loop
+
+const numbersCheck = [53,13,135,52,12,67,34,12]
+for (const num of numbersCheck){
+  console.log(num)
+}
+
 
