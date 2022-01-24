fa4501ef44baf867fd50ef47fe907decd17e859f
marufsiddique01 <marufsiddique71@yahoo.com>
2022-01-24 09:49:15 +0600

    loop tasks

--- 05-loops/loops.js
+++ 05-loops/loops.js
@@ -35,4 +35,30 @@ for (const num of numbersCheck){
   console.log(num)
 }
 
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+console.log(`Iterate 0 to 10 using for loop, do the same using while and do while loop`)
+for (let z=0; z<=10; z++){
+  console.log(z)
+}
+let x =0
+while(x<=10){
+  console.log(x)
+  x++
+}
+let y = 0;
+do{
+  console.log(y)
+  y++
+}while(y<=10)
 
+
+console.log(`Use for loop to iterate from 0 to 100 and print the sum of all numbers.`)
+
+var sum = 0;
+let m =0;
+
+for(i=0; i<=100;i++){
+sum = i + sum;
+}
+
+console.log(sum)
