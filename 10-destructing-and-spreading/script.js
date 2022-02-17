937d35228a2e56c93ab638014b92ce4c3736c2ae
Maruf Siddique <marufsiddique71@yahoo.com>
2022-02-18 00:02:15 +0600

    destructing

--- 10-destructing-and-spreading/script.js
+++ 10-destructing-and-spreading/script.js
@@ -5,3 +5,34 @@ const numbers = [ 1,2,3]
 let [numOne, numTwo, numThree] = numbers
 
 console.log(numOne)
+
+
+// Destructing during iteration
+
+const countries = [['Finland','Helsinki'],['Sweden','Stockholm'],['Bangladesh','Dhaka'],['Norway', 'Oslo']]
+
+for(const [country, city] of countries){
+console.log(country, city)
+}
+
+
+const fullStack = [['HTML','CSS','JS','React'],['Node','Express','MongoDB']]
+for(const [first, second, third] of fullStack){
+console.log(first, second, third)
+}
+
+
+
+// Destructing Object
+
+const rectangular = {
+width: 20,
+height: 10,
+area: 200
+}
+
+let {width, height, area, perimeter} = rectangular
+
+console.log(width, height, area, perimeter)   // missing object element will return undefined
+
+
