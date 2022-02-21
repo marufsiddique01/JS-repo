c27b3c3ab34096764a6060a12ade9b5a6b37fa22
Maruf Siddique <marufsiddique71@yahoo.com>
2022-02-21 16:32:28 +0600

    rest and spread operator

--- 10-destructing-and-spreading/script.js
+++ 10-destructing-and-spreading/script.js
@@ -51,7 +51,7 @@ console.log(calculatePerimeter(rect));
 
 // => object parameter with destructuring 
 
-const calculatePerimeterW = ({width, height}) => {
+const calculatePerimeterW = ({ width, height }) => {
   return 2 * (width + height)
 }
 
@@ -72,12 +72,65 @@ const todoList = [
   }
 ]
 
-for (const {task, startTime, completed} of todoList){
+for (const { task, startTime, completed } of todoList) {
   console.log(task, startTime, completed);
 }
 
 // =>>=>>=>=>>=>>=>>=>==>=>>
 
-// spread / REST operator
+// REST operator
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+let [num1, num2, num3, ...rest] = num
+
+console.log(num1, num2, num3);
+console.log(rest);
+
+// =>>=>>=>=>>=>>=>>=>==>=>>
+
+// spread operator to copy array
+
+const evens = [0, 2, 4, 6, 8, 10]
+const evenNumbers = [...evens]
+
+const odd = [1, 3, 5, 7, 9, 11]
+const oddNumbers = [...odd]
+
+const wholeNumbers = [...evens, ...odd]
+
+console.log(evens);
+console.log(odd);
+console.log(wholeNumbers);
+
+// spread operator to copy object
+
+const user = {
+  name: 'Maruf Siddique',
+  title: 'Programmer',
+  country: 'Bangladesh',
+  city: 'Dhaka'
+}
+
+const copyUser = { ...user }
+console.log(copyUser);
+
+// changing values while spreading the objects
+const testUser = {...user, title:'System Analyst'}
+console.log(testUser);
 
 
+// => spread operator in the arrow function 
+
+const sumAllNums = (...args) => {
+  // console.log(args);
+  let sum = 0;
+  for (const num of args){
+    sum += num;
+  }
+  return sum;
+}
+
+// sumAllNums(1,2,3,4,5)
+
+console.log(sumAllNums(1,2,3,4,5,6,2,2,3,1));
