0575d0b084f244c476e83017e83b7d9443a97916
Maruf Siddique <marufsiddique71@yahoo.com>
2022-02-25 17:26:19 +0600

    all times of console  methods

---
+++ 12-Console-object-methods/script.js
@@ -0,0 +1,30 @@
+// log
+
+
+console.log('Test');
+
+// warn
+console.warn('This is warning')
+
+// error
+console.error('This is an error message')
+
+// table
+const names = ['Maruf', 'Murad', 'Samanta']
+console.table(names)
+
+// // time
+// const sum = 2+3
+// console.timeEnd(sum)
+
+// info
+
+console.info('test')
+
+// assert
+
+// group
+
+// count
+
+// clear
