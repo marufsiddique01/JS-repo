f3684ba6bf97e2402a14860378d01971b5f6afaa
Maruf Siddique <marufsiddique71@yahoo.com>
2022-02-27 23:52:03 +0600

    need to study more about JSON

--- 15-JSON/script.js
+++ 15-JSON/script.js
@@ -1,1 +1,29 @@
-console.log("yesssss");
+
+// JSON 
+// JSON is a JavaScript Object Notation.
+// JSON is a lightweight data-interchange format.
+// JSON is a text format.
+// JSON is a data format.
+// JSON is a data-interchange format.
+// JSON is used to exchange data between programs.
+// JSON is easier to use than XML.
+
+const usersOfAPI = {
+  "users": [
+    {
+      "name": "John",
+      "age": 30,
+      "email": "test@test.com"
+    },
+    {
+      "name": "Jane",
+      "age": 25,
+      "email": "demo@demo.com"
+    }
+  ]
+}
+
+console.log(usersOfAPI);
+
+const userObject = JSON.parse(usersOfAPI);
+console.log(userObject);
