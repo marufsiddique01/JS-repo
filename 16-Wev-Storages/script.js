d15596e6930656c974fa4c5dd2e0add3d2ab7e59
Maruf Siddique <marufsiddique71@yahoo.com>
2022-02-28 00:01:25 +0600

    web storage done

--- 16-Wev-Storages/script.js
+++ 16-Wev-Storages/script.js
@@ -2,11 +2,16 @@
 
 // setting something in the local storage
 localStorage.setItem('firstName', 'Maruf')
-
+localStorage.setItem('lastName', 'Siddique')
 // getting something from the local storage
 const dataFromLocalStorage = localStorage.getItem('firstName')
 console.log(dataFromLocalStorage);
 
+// removing something from the local storage
+console.log(localStorage);
+localStorage.removeItem('firstName')
+console.log(localStorage);
+// clearing something from the local storage
 localStorage.clear()
-console.log(localStorage.getItem('firstName'));
+console.log(localStorage);
 
