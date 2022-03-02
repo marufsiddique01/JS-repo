0d7f660182225d92da5442ab8a8c6d152ba20862
Maruf Siddique <marufsiddique71@yahoo.com>
2022-03-02 19:55:13 +0600

    Dom Day 1 starts

---
+++ 20-DOM/script.js
@@ -0,0 +1,20 @@
+// Document Object Model (DOM) - Day 1
+// Getting Element
+// Getting elements by tag name
+// Getting elements by class name
+// Getting an element by id
+// Getting elements by using querySelector methods
+// Adding attribute
+// Adding attribute using setAttribute
+// Adding attribute without setAttribute
+// Adding class using classList
+// Removing class using remove
+// Adding Text to HTML element
+// Adding Text content using textContent
+// Adding Text Content using innerHTML
+// Text Content
+// Inner HTML
+// Adding style
+// Adding Style Color
+// Adding Style Background Color
+// Adding Style Font Size
