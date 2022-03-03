73567914f398ac7cb13f1383e59550897d9e959d
Maruf Siddique <marufsiddique71@yahoo.com>
2022-03-03 12:29:08 +0600

    done with fetch

--- 17-Promises/script.js
+++ 17-Promises/script.js
@@ -45,10 +45,54 @@ doPromise.then(result => {
 
 // Fetch API
 
+const url = 'https://restcountries.com/v2/all' // countries api
+fetch(url)
+  .then(response => response.json()) // accessing the API data as JSON
+  .then(data => {
+    // getting the data
+    console.log(data)
+  })
+  .catch(error => console.error(error)) // handling error if something wrong happens
 
+
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+fetch(catsAPI).then(res => res.json()).then(data => {
+  console.log(data);
+}).catch(error => console.log(error))
 
 // Async/Await
+// Async and await is an elegant way to handle promises. It is easy to understand and it clean to write.
+// The word async in front of a function means that function will return a promise. The above square function instead of a value it returns a promise.
 
 
+// const square = async function (n) {
+//   return n * n
+// }
+// const value = await square(2)
+// console.log(value)
 
 
+// Using promises ->> on the value of country api
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.error(error))
+
+
+  // using async await -> on the value of country api
+  
+  const fetchData = async () => {
+    try {
+      // console.log('===== async and await')
+      const response = await fetch(url)
+      const countries = await response.json()
+      console.log(countries)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  // console.log('===== async and await')
+  fetchData()
