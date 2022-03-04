105f05f50cfcd7d9f4a8ba375598cddc9a7703c3
Maruf Siddique <marufsiddique71@yahoo.com>
2022-03-04 20:31:08 +0600

    mad over dom

--- 22-Event Listeners/script.js
+++ 22-Event Listeners/script.js
@@ -1,1 +1,24 @@
 console.log("Event Listeners");
+
+// DOM(Document Object Model)-Day 3
+// Event Listeners
+
+// Click
+
+const button = document.querySelector('button')
+// button.addEventListener('click', e => {
+//   console.log(
+//     'e.target.textContent gives content of selected element: ',
+//     e.target.textContent
+//   )
+// })
+
+
+// Double Click
+// Mouse enter
+// Getting value from an input element
+// input value
+// input event and change
+// blur event
+// keypress, keydow and keyup
+
