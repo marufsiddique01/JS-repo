0fbd8a605132aa56a411c3f2f4142881a9166217
marufsiddique01 <marufsiddique71@yahoo.com>
2022-06-14 14:12:23 +0600

    basic js drum kit

--- 50-Drums-js/drums.js
+++ 50-Drums-js/drums.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e){
   // selecting the audio element of the drums using the custom attribute tag
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   // selecting the key values from the drum kit using the class name.
@@ -7,5 +7,22 @@ window.addEventListener('keydown', function(e) {
   if(!audio) return;  // stop the function from running all together
   audio.currentTime = 0; // rewind the playing auto to the start
   audio.play();
-  console.log(key)
-})
+  // console.log(key)
+  // adding playing class
+  key.classList.add('playing');
+}
+
+
+window.addEventListener('keydown', playSound)
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+// Selecting all the keys
+const keys = document.querySelectorAll('.key');
+// console.log(keys)
+// each key of the keys list, will have a remove transition class
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
