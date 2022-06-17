68a3e3a72529f1b25fa35f8992e936b7f30f6cc4
Maruf Siddique <marufsiddique71@yahoo.com>
2022-06-17 08:31:02 +0600

    done with image filter add

--- 52-CSS-variables/script.js
+++ 52-CSS-variables/script.js
@@ -1,1 +1,17 @@
 // console.log("yesssss");
+const inputs = document.querySelectorAll('.controls input')
+// console.log(inputs)
+
+
+function handleUpdate() {
+  // console.log(this.dataset);
+  // // console.log(this.value)
+  const suffix = this.dataset.sizing || '';
+  // console.log(suffix);
+  // // console.log(this.name);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value+ suffix)
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
